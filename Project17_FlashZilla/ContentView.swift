290033e6410de839eb912091d6663d040202de4f
suhail <suhailajaz338@gmail.com>
2024-12-29 13:37:57 +0530

    Project Created

---
+++ Project17_FlashZilla/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Project17_FlashZilla
+//
+//  Created by suhail on 27/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View{
+        
+        Text("hello")
+    }
+}
+#Preview {
+    ContentView()
+}
---
+++ Project17_FlashZilla/Project17_FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project17_FlashZillaApp.swift
+//  Project17_FlashZilla
+//
+//  Created by suhail on 27/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Project17_FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
